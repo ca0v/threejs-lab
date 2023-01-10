f822d6f6c1b1b9c2164b04b271e358cb46af0b8f
Corey Alix <coreyalix@gmail.com>
2023-01-10 17:02:18 -0500

    Playing with parameters

--- src/main.ts
+++ src/main.ts
@@ -16,12 +16,14 @@ function readCssVariable(name: string) {
 
 function createScene() {
   const cubeColor = readCssVariable("cube-color")
+  const cubeLineColor = readCssVariable("cube-line-color")
   const { innerWidth: sceneWidth, innerHeight: sceneHeight } = window
   const perspective = sceneWidth / sceneHeight
   const scene = new THREE.Scene()
 
   // define the camera characteristics
   const camera = new THREE.PerspectiveCamera(75, perspective, 0.1, 1000)
+  camera.position.z = 2
 
   // create renderer world size
   const renderer = new THREE.WebGLRenderer()
@@ -29,32 +31,49 @@ function createScene() {
 
   // add smoothing to the edges of the cube
   renderer.setPixelRatio(window.devicePixelRatio)
-  
+
   app.appendChild(renderer.domElement)
 
   // create a cube
-  const geometry = new THREE.BoxGeometry()
-  const material = new THREE.MeshBasicMaterial({ color: cubeColor })
-  const cube = new THREE.Mesh(geometry, material)
-  // draw lines along the edges of the cube
-  const edges = new THREE.EdgesGeometry(geometry)
-  const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0x000000 }))
-  cube.add(line)
+  {
+    const geometry = new THREE.BoxGeometry(1.5, 1, 1)
+    const material = new THREE.MeshBasicMaterial({ color: cubeColor })
+    const cube = new THREE.Mesh(geometry, material)
 
-  scene.add(cube)
+    // draw lines along the edges of the cube using
+    const edges = new THREE.EdgesGeometry(geometry)
+    const line = new THREE.LineSegments(
+      edges,
+      new THREE.LineBasicMaterial({ color: cubeLineColor })
+    )
 
-  camera.position.z = 5
+    cube.add(line)
+    scene.add(cube)
 
-  const animate = () => {
-    requestAnimationFrame(animate)
+    const animate = () => {
+      requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+      cube.rotation.x += 0.01
+      cube.rotation.y += 0.01
 
-    renderer.render(scene, camera)
+      renderer.render(scene, camera)
+    }
+
+    animate()
   }
 
-  animate()
+  // rotate the camera around the cube
+  {
+    const animate = () => {
+      requestAnimationFrame(animate)
+
+      camera.position.x = 15 - 15 *  Math.sin(Date.now() / 1000)
+      camera.lookAt(scene.position)
+
+      renderer.render(scene, camera)
+    }
+    animate()
+  }
 }
 
 export { createScene }
